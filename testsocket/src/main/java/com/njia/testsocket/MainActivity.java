e5172d8b0e4895221700284bf031d571f95f28a5
penglei <penglei@fenxianglife.com>
2021-02-19 14:30:44 +0800

    socket

--- testsocket/src/main/java/com/njia/testsocket/MainActivity.java
+++ testsocket/src/main/java/com/njia/testsocket/MainActivity.java
@@ -1,21 +1,16 @@
 package com.njia.testsocket;
 
-import androidx.annotation.MainThread;
-import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
-import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.os.Message;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.njia.testsocket.server.ISocketServiceUtils;
 import com.njia.testsocket.server.PLServiceSocket;
 
 import java.util.Map;
@@ -25,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements PLServiceSocket.C
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((TextView)findViewById(R.id.ipAddess)).setText("本机IP地址："+AddressUtils.getLocalWifiIpAddress(this));
 //        new Thread(new Runnable() {
 //            @Override
 //            public void run() {
